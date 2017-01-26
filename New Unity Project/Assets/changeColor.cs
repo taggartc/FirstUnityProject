eb962eb94757c8c04eb51048ac743538af5bb011
Chase Taggart <ctag23@gmail.com>
2017-01-26 11:40:51 -0700

    Added First Unity Project

---
+++ New Unity Project/Assets/changeColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColor : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			GetComponent<Renderer> ().material.color = Color.red;
+		}
+		if (Input.GetKeyDown (KeyCode.G)) {
+			GetComponent<Renderer> ().material.color = Color.green;
+		}
+		if (Input.GetKeyDown (KeyCode.B)) {
+			GetComponent<Renderer> ().material.color = Color.blue;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Renderer> ().material.color = Color.black;
+		}
+		if (Input.GetKeyDown (KeyCode.Delete)) {
+			GetComponent<Renderer> ().material.color = Color.white;
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			GetComponent<Renderer> ().material.color = Color.yellow;
+		}
+	}
+}
