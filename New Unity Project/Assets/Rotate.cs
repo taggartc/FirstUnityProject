e09fda2034232f66f08b5c0b9286bcb9aee0708f
Chase Taggart <ctag23@gmail.com>
2017-01-31 11:09:26 -0700

    Added class feature and scripts for rotate and move

---
+++ New Unity Project/Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (.5f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 45, 0) * Time.deltaTime * speed);
+	}
+}
